a755433d05ecc8d06b98331cc4afee41ea45b2be
jphwang212 <jphwang212@gmail.com>
2024-12-14 16:29:37 -0500

    completed char frequency map

--- compression_tool.cpp
+++ compression_tool.cpp
@@ -1,20 +1,44 @@
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <sys/stat.h>
 
 using namespace std;
 
-static void readFile(string file) {
+static void readFile(string file, unordered_map<char, int> *charFreq) {
     struct stat buffer;
+    char ch;
     int code = stat(file.c_str(), &buffer);
-    if (code == 0) {
-        cout << "File is valid.\n";
-    } else {
-        cout << "File is NOT valid.\nCode " << code;
+    if (code != 0) {
+        cout << "File is not valid.\ncode " << code << "\n";
+        return;
+    }
+    std::locale::global(std::locale(""));
+    std::ifstream readFile(file);
+    if (readFile.is_open()) {
+        while (readFile.get(ch)) {
+            if (ch == ' ' || ch == '\n' || ch == '\'') {
+                continue;
+            }
+            if ((*charFreq).find(ch) == (*charFreq).end()) {
+                charFreq->insert({ch, 1});
+            } else {
+                charFreq->at(ch)++;
+            }
+        }
+    }
+}
+
+static void printCharFrequency(unordered_map<char, int> *charFreq) {
+    cout << "*******Char Frequency*******\n";
+    for (const auto &pair : *charFreq) {
+        cout << pair.first << ": " << pair.second << "\n";
     }
 }
 
 int main() {
-    readFile("file");
+    unordered_map<char, int> charFrequency = unordered_map<char, int>();
+    readFile("test.txt", &charFrequency);
+    printCharFrequency(&charFrequency);
     return 0;
 }
